161b4f8d9597244ed943193011676d5b1b38057e
igor <cujbaigor90@gmail.com>
2022-09-11 00:36:45 +0300

    feat(v2): add select with regions and buttons

--- v2/script.js
+++ v2/script.js
@@ -1,12 +1,14 @@
-function snail(_x, _y) {
+function snail(_x, _y, hash = undefined) {
     const x = _x;
     const y = _y;
 
     return function (n) {
         if (this.data === undefined) {
-            this.data = Array();
+            console.log('reset');
+            this.data = {};
         }
-        if (this.data[n] === undefined) {
+        const name = `${hash}${n}`;
+        if (this.data[name] === undefined) {
             let add = { x: 0, y: 0 };
             switch (n) {
                 case 1:
@@ -39,9 +41,9 @@ function snail(_x, _y) {
                 default:
                     throw new Error(`Something wong wrong[${n}]`);
             }
-            this.data[n] = add;
+            this.data[name] = add;
         }
-        return this.data[n];
+        return this.data[name];
     };
 }
 
@@ -63,19 +65,6 @@ const areaSize = {
     width: area[0].length
 };
 
-const names = {
-    r: snail(12, 9)
-};
-// const from = (n) => {
-//     console.log('n', n);
-//     const name = names[n];
-//     console.log('name', name);
-//     return {
-//         x: name.x * ts,
-//         y: name.y * ts
-//     };
-// };
-
 async function loadImage(src) {
     return new Promise((response, reject) => {
         var image = new Image();
@@ -88,6 +77,41 @@ async function loadImage(src) {
     });
 }
 
+const regions = [
+    { x: 12, y: 2 },
+    { x: 15, y: 2 },
+    { x: 18, y: 2 },
+    { x: 21, y: 2 },
+    { x: 24, y: 2 },
+    { x: 27, y: 2 },
+    { x: 12, y: 8 },
+    { x: 15, y: 8 },
+    { x: 18, y: 8 },
+    { x: 21, y: 8 },
+    { x: 24, y: 8 },
+    { x: 0, y: 11 },
+    { x: 3, y: 11 },
+    { x: 6, y: 11 },
+    { x: 9, y: 11 },
+    { x: 5, y: 17 },
+    { x: 8, y: 21 },
+    { x: 0, y: 22 },
+    { x: 3, y: 22 },
+    { x: 0, y: 28 },
+    { x: 3, y: 28 },
+    { x: 6, y: 26 },
+    { x: 16, y: 23 },
+    { x: 19, y: 23 },
+    { x: 16, y: 26 },
+    { x: 20, y: 26 },
+    { x: 16, y: 29 },
+    { x: 21, y: 29 }
+];
+
+const max = regions.length - 1;
+
+let drawWithRegion;
+
 function draw(tiles) {
     const width = ts * areaSize.width;
     const height = ts * areaSize.height;
@@ -95,64 +119,48 @@ function draw(tiles) {
     canvas.width = width;
     canvas.height = height;
     const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, width, height);
 
-    // const ground = snail(12, 8); // dark sand
-    // const ground = snail(15, 8); // lava
-    // const ground = snail(18, 8); // coal
-    // const ground = snail(21, 8); // grass
-    const ground = snail(24, 8); // beton
-    for (let y = 0; y < areaSize.height; y++) {
-        for (let x = 0; x < areaSize.width; x++) {
-            const a = area[y][x];
-            ctx.drawImage(tiles, ground(a).x * ts, ground(a).y * ts, ts, ts, x * ts, y * ts, ts, ts);
+    drawWithRegion = function (n) {
+        n = Math.min(Math.max(0, n), max);
+        select.value = n;
+        console.log('region:', n);
+        const zeroTail = regions[n];
+        const ground = snail(zeroTail.x, zeroTail.y, n);
+        ctx.clearRect(0, 0, width, height);
+
+        for (let y = 0; y < areaSize.height; y++) {
+            for (let x = 0; x < areaSize.width; x++) {
+                const a = area[y][x];
+                ctx.drawImage(tiles, ground(a).x * ts, ground(a).y * ts, ts, ts, x * ts, y * ts, ts, ts);
+            }
         }
-    }
+    };
 
-    // --------------------------------------------------
-    // ctx.drawImage(tiles, ground(1).x * ts, ground(1).y * ts, ts, ts, 0 * ts, 0 * ts, ts, ts);
-    // const top = ground(2);
-    // const down = ground(6);
-    // for (let i = 1; i < areaSize.width - 1; i++) {
-    //     ctx.drawImage(tiles, top.x * ts, top.y * ts, ts, ts, i * ts, 0 * ts, ts, ts);
-    //     ctx.drawImage(tiles, down.x * ts, down.y * ts, ts, ts, i * ts, (areaSize.height - 1) * ts, ts, ts);
-    // }
-    // ctx.drawImage(tiles, ground(3).x * ts, ground(3).y * ts, ts, ts, (areaSize.width - 1) * ts, 0 * ts, ts, ts);
+    const region = Math.floor(max * Math.random());
+    drawWithRegion(region);
 
-    // const right = ground(4);
-    // const left = ground(8);
-    // for (let i = 1; i < areaSize.height - 1; i++) {
-    //     ctx.drawImage(tiles, right.x * ts, right.y * ts, ts, ts, (areaSize.width - 1) * ts, i * ts, ts, ts);
-    //     ctx.drawImage(tiles, left.x * ts, left.y * ts, ts, ts, 0 * ts, i * ts, ts, ts);
-    // }
-    // ctx.drawImage(tiles, ground(5).x * ts, ground(5).y * ts, ts, ts, (areaSize.width - 1) * ts, (areaSize.height - 1) * ts, ts, ts);
-    // ctx.drawImage(tiles, ground(7).x * ts, ground(7).y * ts, ts, ts, 0 * ts, (areaSize.height - 1) * ts, ts, ts);
-
-    // --------------------------------------------------
-    // ctx.drawImage(tiles, ground(2).x * ts, ground(2).y * ts, ts, ts, 2 * ts, 0 * ts * 2, ts, ts);
-
-    //     for (let y = 0; y < areaSize.height; y++) {
-    //         for (let x = 0; x < areaSize.width; x++) {
-    //             const s = from(area[y][x]);
-    //             console.log('source', s);
-    //             ctx.drawImage(tiles, s.x, s.y, ts, ts, x * ts, y * ts, ts, ts);
-    //         }
-    //     }
-    //     // water
-    //     ctx.drawImage(tiles, 21 * ts, 17 * ts, 3 * ts, 1 * ts, 100 - ts * 2, 100 - ts * 2, 3 * ts, 1 * ts);
-    //     ctx.drawImage(tiles, 21 * ts, 17 * ts, 2 * ts, 1 * ts, 100 + ts, 100 - ts * 2, 2 * ts, 1 * ts);
-    //     // wall
-    //     ctx.drawImage(tiles, 21 * ts, 22 * ts, 5 * ts, 1 * ts, 100 - ts * 2, 100 - ts * 1, 5 * ts, 1 * ts);
-    //     // waterfall
-    //     ctx.drawImage(tiles, 21 * ts, 12 * ts, 3 * ts, 4 * ts, 100 - ts, 100 - ts, 3 * ts, 4 * ts);
-    //     // rock cup
-    //     ctx.drawImage(tiles, 17 * ts, 12 * ts, 1 * ts, 2 * ts, 100, 100 + ts, 1 * ts, 2 * ts);
-    //     // head
-    //     ctx.drawImage(tiles, 15 * ts, 12 * ts, 2 * ts, 3 * ts, 100 + ts * 2, 100, 2 * ts, 3 * ts);
+    select.addEventListener('input', function (e) {
+        drawWithRegion(select.value);
+    });
+    document.getElementById('up').addEventListener('click', function (e) {
+        drawWithRegion(1 + parseInt(select.value));
+    });
+    document.getElementById('down').addEventListener('click', function (e) {
+        drawWithRegion(-1 + parseInt(select.value));
+    });
 }
 
 // // run
 
-(function () {
+let select;
+window.onload = function () {
+    select = document.getElementById('select');
+    console.log('select', select);
+    for (let i = 0; i <= max; i++) {
+        const option = document.createElement('option');
+        option.setAttribute('value', i);
+        option.innerHTML = i;
+        select.appendChild(option);
+    }
     loadImage('./../img/tiles.png').then(draw);
-})();
+};
